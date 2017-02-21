7431c9a6ce6c7f21043cb698a4b0e39c960e6677
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-02-21 16:46:40 -0700

    changed file strucure and began to build out login

---
+++ app/component.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('app', ['angularMoment'])
+  .component("login", {
+
+    controller: function() {
+      const vm = this;
+
+    },
+    templateUrl: "app/logintemplate.html"
+
+  })
+  .component("signup", {
+
+    controller: function() {
+      const vm = this;
+
+    },
+    templateUrl: "app/signuptemplate.html"
+  })
